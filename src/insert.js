59aeb424eabb4cb130782db043bebc7bd9bd324c
fx4waldi <fx4waldi@yahoo.com>
2017-12-13 19:03:21 +0100

    Version 1.2.7
    - Added support for audio tag #2

--- src/insert.js
+++ src/insert.js
@@ -5,33 +5,47 @@ function mes(m){
 }
 
 function control(e) {
-	let videos=document.getElementsByTagName("video");
-	if(!videos[0])return;
-	let rate=videos[0].playbackRate;
-	let len=videos.length;
+	let videos=document.getElementsByTagName("video"),
+		audios=document.getElementsByTagName("audio");
+	if(!videos[0]&&!audios[0])return;
+	let rate=videos[0]?videos[0].playbackRate:audios[0].playbackRate,
+		lenV=videos.length,
+		lenA=audios.length;
 	switch(e){
 		case "open":
-			for(let i=0;i<len;i++){
+			for(let i=0;i<lenV;i++){
 				browser.runtime.sendMessage({"openTab":true,"url":videos[i].currentSrc});
+			}
+			for(let i=0;i<lenA;i++){
+				browser.runtime.sendMessage({"openTab":true,"url":audios[i].currentSrc});
 			}
 			break;
 		case "plus":
 			let prate=(rate>3.75)?4:rate+0.25;
-			for(let i=0;i<len;i++){
+			for(let i=0;i<lenV;i++){
 				videos[i].playbackRate=prate;
+			}
+			for(let i=0;i<lenA;i++){
+				audios[i].playbackRate=prate;
 			}
 			browser.runtime.sendMessage({"rate":prate});
 			break;
 		case "minus":
 			let mrate=(rate<0.5)?0.25:rate-0.25;
-			for(let i=0;i<len;i++){
+			for(let i=0;i<lenV;i++){
 				videos[i].playbackRate=mrate;
+			}
+			for(let i=0;i<lenA;i++){
+				audios[i].playbackRate=mrate;
 			}
 			browser.runtime.sendMessage({"rate":mrate});
 			break;
 		default:
-			for(let i=0;i<len;i++){
+			for(let i=0;i<lenV;i++){
 				videos[i].playbackRate=e;
+			}
+			for(let i=0;i<lenA;i++){
+				audios[i].playbackRate=e;
 			}
 			browser.runtime.sendMessage({"rate":e});
 	}
--- src/isvideo.js
+++ src/isvideo.js
@@ -1,4 +1,6 @@
 (function(){
-	let videos=document.getElementsByTagName("video");
+	let videos=document.getElementsByTagName("video"),
+		audios=document.getElementsByTagName("audio");
 	if(videos[0])browser.runtime.sendMessage({"isVideo":true,"rate":videos[0].playbackRate});
+	else if(audios[0])browser.runtime.sendMessage({"isVideo":true,"rate":audios[0].playbackRate});
 })();
